4c2ebbc86418814243be5a991bd51ae548977cb2
Julio Paredes <pjulio57@gmail.com>
2018-04-11 01:47:47 -0500

    Made a table and prints out the blogs in the same main page as the form

--- main.py
+++ main.py
@@ -3,7 +3,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogger@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:bloggers@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_content = request.form['content']
+
+        #validation code will go here
+
+        new_blog = Blog(blog_title, blog_content)
+        db.session.add(new_blog)
+        db.session.commit()
+
+    blogs = Blog.query.all()
+
+
+    return render_template('homepage.html', blogs=blogs)
+
+
+if __name__=='__main__':
+    app.run()
